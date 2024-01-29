63c873cd370497b592fa38f8c838615a559c6eda
Nikolay Chernikov <n.chernikov@sxope.com>
2024-01-29 16:41:25 +0300

    feat(currency_pairs_api): add validators and revert compendium for tokens

--- src/currency_pairs_api/endpoints/v1/currency_pairs.py
+++ src/currency_pairs_api/endpoints/v1/currency_pairs.py
@@ -1,8 +1,10 @@
 """ Test endpoint module. """
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse, Response
 
 from src.currency_pairs_api.services.currency_pairs import CurrencyPairsService
+from starlette.status import HTTP_404_NOT_FOUND
 
 currency_pairs_router = APIRouter()
 
@@ -17,7 +19,14 @@ async def get_currency_pairs(
         token: str,
         currency: str,
         currency_pairs: CurrencyPairsService = Depends(Provide["currency_pairs"])  # noqa
-) -> dict:
+):
     res = currency_pairs.execute(token, currency)
 
-    return res
+    if not res:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail={
+                "message": "Pair not found",
+            }
+        )
+    return JSONResponse(res)
--- src/currency_pairs_api/services/currency_pairs.py
+++ src/currency_pairs_api/services/currency_pairs.py
@@ -2,6 +2,8 @@
 import logging
 from src.lib.repositories.currency_pairs import CurrencyPairsRepository
 from src.lib.postgres.database import PostgresDatabase
+from fastapi import HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 __all__ = ("CurrencyPairsService",)
 
@@ -14,10 +16,47 @@ class CurrencyPairsService:
     currency_pairs_repository: "CurrencyPairsRepository"
     db_postgres: "PostgresDatabase"
 
+    def __init__(self) -> None:
+        self.token_compendium = {
+            'btc': 'bitcoin',
+            'eth': 'ethereum',
+        }
+        self.invert_token_compendium = {v: k for k, v in self.token_compendium.items()}
+        self.currency_compendium = ['rub', 'usd']
+
     def execute(self, token, currency):
+        self.validate_pair(token, currency)
         with self.db_postgres.session() as session:
             result = self.currency_pairs_repository.get_row(session=session,
-                                                            token=token,
-                                                            currency=currency)
+                                                            token=self.token_compendium[token.lower()],
+                                                            currency=currency.lower())
             if result:
-                return self.currency_pairs_repository.model_as_dict(result)
+                dict_result = self.currency_pairs_repository.model_as_dict(result)
+                return self.format_msg(dict_result['token'], dict_result['currency'], dict_result['value'])
+
+    def validate_pair(self, token: str, currency: str) -> None:
+        if token.lower() not in self.token_compendium.keys():
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND,
+                detail={
+                    "message": f"Token not found. Available tokens: {list(self.token_compendium.keys())}",
+                }
+            )
+        elif currency.lower() not in self.currency_compendium:
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND,
+                detail={
+                    "message": f"Currency not found. Available currencies: {self.currency_compendium}",
+                }
+            )
+
+    def format_msg(self, token: str, currency: str, value: float):
+        return {
+            "exchanger": "coingecko",
+            "cources": [
+                {
+                    "direction": f"{self.invert_token_compendium[token.lower()]}-{currency}".upper(),
+                    "value": value
+                }
+            ]
+        }
